f1a8daef81a988a778f9448d8cb6634d0091b6db
mzguido <martinez.guido14@gmail.com>
2023-01-15 20:18:12 -0300

    fix: product filter by category enabled

--- src/Product/Infrastructure/Repositories/ProductMongooseRepository.ts
+++ src/Product/Infrastructure/Repositories/ProductMongooseRepository.ts
@@ -1,4 +1,6 @@
-import { Query } from 'mongoose';
+import { Query,Schema, model  } from 'mongoose';
+import { connection } from '../../../Shared/Infrastructure/Database/CreateMongooseConnection';
+
 import ICriteria from '../../../Shared/Presentation/Requests/ICriteria';
 import IPaginator from '../../../Shared/Infrastructure/Orm/IPaginator';
 
@@ -11,6 +13,9 @@ import BaseMongooseRepository from '../../../Shared/Infrastructure/Repositories/
 import IProductDomain from '../../Domain/Entities/IProductDomain';
 import Product from '../../Domain/Entities/Product';
 
+import ICategoryDomain from '../../../Category/Domain/Entities/ICategoryDomain';
+import Category from '../../../Category/Domain/Entities/Category';
+
 class ProductMongooseRepository extends BaseMongooseRepository<IProductDomain, IProduct> implements IProductRepository
 {
     constructor()
@@ -20,6 +25,10 @@ class ProductMongooseRepository extends BaseMongooseRepository<IProductDomain, I
 
     async list(criteria: ICriteria): Promise<IPaginator>
     {
+        // Get all enabled categories
+        const categoryRepository = connection.model<ICategoryDomain>(Category.name);
+        let enabledCategories = (await categoryRepository.find({ enable: true })).map(cat => cat._id)
+
         const queryBuilder: Query<IProduct[], IProduct> = this.repository.find();
         const filter = criteria.getFilter();
 
@@ -39,14 +48,6 @@ class ProductMongooseRepository extends BaseMongooseRepository<IProductDomain, I
             void queryBuilder.where(ProductFilter.TITLE).regex(rSearch);
         }
 
-        if (filter.has(ProductFilter.PRICE))
-        {
-            const title: string = filter.get(ProductFilter.TITLE) as string;
-            const rSearch = new RegExp(title, 'g');
-
-            void queryBuilder.where(ProductFilter.TITLE).regex(rSearch);
-        }
-
          if (filter.has(ProductFilter.PRICE))
         {
             const price = filter.get(ProductFilter.PRICE);
@@ -54,6 +55,16 @@ class ProductMongooseRepository extends BaseMongooseRepository<IProductDomain, I
             void queryBuilder.where(ProductFilter.PRICE).equals(price);
         }
 
+        if (filter.has(ProductFilter.CATEGORY_ENABLE))
+        {
+            const _enable = filter.get(ProductFilter.CATEGORY_ENABLE) as string;
+            const enable: boolean = _enable !== 'false';
+            if (enable) {
+                void queryBuilder.where('category').in(enabledCategories);
+            } else { 
+                void queryBuilder.where('category').nin(enabledCategories);
+            }
+        }
         void queryBuilder.populate(this.populate);
 
         return new MongoosePaginator(queryBuilder, criteria);
--- src/Product/Presentation/Criterias/ProductFilter.ts
+++ src/Product/Presentation/Criterias/ProductFilter.ts
@@ -2,22 +2,27 @@ import Filter from '../../../Shared/Presentation/Requests/Filter';
 
 class ProductFilter extends Filter
 {
-  static readonly TITLE: string = 'title';
-  static readonly PRICE: string = 'price';
-  static readonly ENABLE: string = 'enable';
-//   category: string;
+    static readonly TITLE: string = 'title';
+    static readonly PRICE: string = 'price';
+    static readonly ENABLE: string = 'enable';
+
+    // filter[catEnalbe]=false will show products that are not in a category and products that are in a disabled category
+    static readonly CATEGORY_ENABLE: string = 'catEnable';
 
     getFields(): any
     {
         return [
             ProductFilter.TITLE,
-            ProductFilter.ENABLE
+            ProductFilter.ENABLE,
+            ProductFilter.CATEGORY_ENABLE
         ];
     }
 
     getDefaultFilters(): any
     {
-        return [];
+        return [
+           { [ProductFilter.CATEGORY_ENABLE]: true}
+        ];
     }
 }
 
